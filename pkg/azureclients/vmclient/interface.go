2eaa40bb7958b3bfb0e859bbcd78a6681d20d84e
Ed Reed <edreed@microsoft.com>
2022-05-09 11:25:32 -0700

    chore: Use API version 2021-07-01

--- pkg/azureclients/vmclient/interface.go
+++ pkg/azureclients/vmclient/interface.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	// APIVersion is the API version for VirtualMachine.
-	APIVersion = "2020-12-01"
+	APIVersion = "2021-07-01"
 	// AzureStackCloudAPIVersion is the API version for Azure Stack
 	AzureStackCloudAPIVersion = "2017-12-01"
 	// AzureStackCloudName is the cloud name of Azure Stack
